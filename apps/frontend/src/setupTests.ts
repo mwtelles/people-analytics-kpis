4407924a0462159c2f8308f8174c2a9f25048e94
Matheus Telles <business.mwtelles@gmail.com>
2025-08-29 02:49:47 -0300

    fix: lint

--- apps/frontend/src/setupTests.ts
+++ apps/frontend/src/setupTests.ts
@@ -10,7 +10,6 @@ declare global {
   interface Window {
     ResizeObserver: typeof ResizeObserverMock;
   }
-  interface Global extends Window {}
 }
 
 global.ResizeObserver = ResizeObserverMock as unknown as typeof ResizeObserver;
